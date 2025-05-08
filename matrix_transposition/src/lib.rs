e231e3510b4c425806e256ca9d85d0f01a14178c
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-08 16:04:20 +0100

    good

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,7 +1,9 @@
-#[derive(Debug)]
+#[derive(Debug , PartialEq, Eq)]
 pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    let new_m = Matrix((m.0.0 , m.1.0) , (m.0.1 , m.1.1));
-    new_m
+     Matrix(
+        (m.0.0 , m.1.0) , (m.0.1 , m.1.1)
+    )
+    
 }

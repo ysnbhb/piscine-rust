e772c265b3cd8990e4d4c46cb5a316f8c3be33f9
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-08 13:21:47 +0100

    good

---
+++ groceries/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    slice.get(index).expect("not found")
+}
+

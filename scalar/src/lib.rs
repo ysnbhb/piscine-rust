b0167bb67d6c6195f5e68e10213a8793b0517f63
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-08 12:12:53 +0100

    good

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn add(left: u8, right: u8) -> u8 {
+    left + right
+}
+
+pub fn deff(left: i16, right: i16) -> i16 {
+    left - right
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
+}
+
+pub fn quo(a: f32, b: f32) -> f32 {
+    a / b
+}
+
+pub fn rem(a: f32, b: f32) -> f32 {
+    a%b
+}

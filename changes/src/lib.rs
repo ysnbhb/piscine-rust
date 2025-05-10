d1e64e079f613a5cd39e0c96e54d66e3bb6dcc08
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-10 17:02:53 +0100

    good

---
+++ changes/src/lib.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+    pub alias: String,
+    pub brightness: u8,
+}
+
+impl Light {
+    pub fn new(alias: &str) -> Self {
+        Light {
+            alias: alias.to_string(),
+            brightness: 0,
+        }
+    }
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for i in lights {
+        if i.alias == alias {
+            i.brightness = value
+        }
+    }
+}

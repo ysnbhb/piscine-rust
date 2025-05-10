3877eb5cb5b27e4a3158914adce8306674ebcb99
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-10 18:27:15 +0100

    good

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,58 @@
+use std::ptr::null;
+
+use rand::Rng;
+
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+pub enum Rank {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Number(u8),
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let ransm = rand::thread_rng().gen_range(1..5);
+        Suit::translate(ransm)
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let ransm = rand::thread_rng().gen_range(1..14);
+        Rank::translate(ransm)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            _ => Rank::King,
+        }
+    }
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {}

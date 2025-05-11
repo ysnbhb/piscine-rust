c3f3829dd06637d82423f5b2c908f3c3c8fa64fb
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-11 15:09:39 +0100

    good

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -5,22 +5,14 @@ pub fn mean(list: &[i32]) -> f64 {
 }
 
 pub fn median(list: &[i32]) -> i32 {
-    let mut new_list = Vec::new();
-    let mut map = HashMap::new();
-    let len = list.len();
-    for &word in list {
-        *map.entry(word).or_insert(0) += 1;
-    }
-    for (v, _) in map {
-        new_list.push(v);
-    }
-    new_list.sort(); 
-    println!("{:?}", new_list);
-    if new_list.len() % 2 == 1 {
-        new_list[new_list.len() / 2]
+    let mut new_list = list.to_vec();
+    new_list.sort();
+    let len = new_list.len();
+
+    if len % 2 == 1 {
+        new_list[len / 2]
     } else {
         ((new_list[len / 2 - 1] as f32 + new_list[len / 2] as f32) / 2.0).round() as i32
-
     }
 }
 

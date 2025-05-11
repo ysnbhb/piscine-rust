4819eeae6fcba237cf142d35f323052a7ffecb0c
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-11 15:56:49 +0100

    good

--- bigger/src/lib.rs
+++ bigger/src/lib.rs
@@ -1,15 +1,4 @@
-use std::{collections::HashMap, f32::INFINITY};
+use std::collections::HashMap;
 pub fn bigger(h: HashMap<&str, i32>) -> i32 {
-
-    let mut big = -INFINITY as i32;
-    for (_ , num) in h {
-        big = if num > big {
-            num
-        }else {
-            big
-        };
-       
-    }
-    big
-
+    h.values().cloned().max().unwrap()
 }

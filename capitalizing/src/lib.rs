1f16e2b35b3577f48ef548d22e5c5171bc67b528
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-11 10:52:47 +0100

    good

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -2,24 +2,29 @@ pub fn capitalize_first(input: &str) -> String {
     if input.is_empty() {
         return String::new();
     }
-   let new  = input.split_at(1);
-   new.0.to_uppercase() + &new.1.to_lowercase()
+    let new = input.split_at(1);
+    new.0.to_uppercase() + new.1
 }
 
 pub fn title_case(input: &str) -> String {
     let mut new_strin = Vec::new();
-    for i in input.split(" "){
+    for i in input.split(" ") {
         new_strin.push(capitalize_first(i));
     }
-    new_strin.join(" ")
+    new_strin.join(" ");
+    let mut new = Vec::new();
+    for i in input.split("\t") {
+        new.push(capitalize_first(i));
+    }
+    new.join(" ")
 }
 
 pub fn change_case(input: &str) -> String {
-    let mut new_string= String::new();
+    let mut new_string = String::new();
     for i in input.chars() {
-        new_string.push(if i.is_uppercase()  {
+        new_string.push(if i.is_uppercase() {
             i.to_ascii_lowercase()
-        }else {
+        } else {
             i.to_ascii_uppercase()
         });
     }

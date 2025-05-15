879217a649dd93d03fc1e61b2635f5ad48db3176
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-15 12:53:38 +0100

    good

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,26 @@
+#[derive(PartialEq, Eq)]
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+use crate::Security::*;
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    if security_level == Message {
+        return server.unwrap_or("ERROR: program stops").to_string();
+    } else if security_level == Unknown {
+        return server.unwrap().to_string();
+    } else if security_level == Warning {
+        return server.expect("WARNING: check the server").to_string();
+    } else if security_level == NotFound {
+        server
+            .map(String::from)
+            .unwrap_or_else(|f| format!("Not found: {}", f))
+    } else {
+        server.unwrap_err().to_string()
+    }
+}

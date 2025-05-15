983fa187b496c5fa84a0408eddbded73e9b2a304
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-15 14:15:14 +0100

    good

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs::OpenOptions, io::Write, path::Path};
+use std::{fs::OpenOptions, io::{Read, Write}, path::Path};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let mut file = OpenOptions::new()
@@ -6,6 +6,13 @@ pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
         .write(true)
         .create(true)
         .open(path).unwrap();
-    file.write(content.as_bytes()).unwrap();
+    let mut last_content  = vec![];
+    let _ = file.read_to_end(&mut last_content);
+    for i in content.as_bytes() {
+        last_content.push(*i);
+
+    }
+    file.write(&last_content).unwrap();
+    // file.write_all(content.as_bytes()).unwrap();
     
 }

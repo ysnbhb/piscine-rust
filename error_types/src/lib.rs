8c2ed7db47ef848c01ec3e8fbbf8adcc255cd6b7
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-16 10:55:18 +0100

    good

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -11,7 +11,7 @@ impl FormError {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
         Self {
             form_values: (field_name, field_value),
-            date: Utc::now().format("%Y-%M-%D %H:%M:%S").to_string(),
+            date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
             err: err,
         }
     }

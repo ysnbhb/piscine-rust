738e46cd7fb19b418565b32cfff59ba901b2bd50
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-17 12:00:15 +0100

    good

---
+++ pangram/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+pub fn is_pangram(s: &str) -> bool {
+    for i in s.split_ascii_whitespace() {
+        let mut res = false;
+        let mut mp: HashMap<char, bool> = HashMap::new();
+        for j in i.chars() {
+            res = *mp.get(&j).unwrap_or(&false);
+            if res {
+                return false;
+            }
+            mp.insert(j, true);
+        }
+    }
+    true
+}

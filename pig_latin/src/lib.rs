42392fb0abe2ebaeb7f7016225e56b4353ec95c9
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-17 15:18:06 +0100

    goo

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,13 +1,14 @@
 pub fn pig_latin(text: &str) -> String {
     let mut res = String::from(text);
     let i = "aeiou";
-    for j in text.chars() {
-        if res.starts_with("qu") {
+
+    for (j, c) in text.chars().enumerate() {
+        if res.starts_with("qu") && j != 0 {
             res = res.chars().skip(2).collect();
             res.push_str("qu");
             break;
         }
-        if i.contains(j) {
+        if i.contains(c) {
             break;
         }
         let c = res.remove(0);

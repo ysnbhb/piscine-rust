8141c5168a9e64191c68f2ea5357ea9a4e134a64
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-17 11:48:53 +0100

    good

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,9 +1,12 @@
 pub fn num_to_ordinal(x: u32) -> String {
+    if x >= 10 && x <= 20 {
+        return x.to_string() + &String::from("th");
+    }
     if x % 10 == 1 {
         return x.to_string() + &String::from("st");
     } else if x % 10 == 2 {
         return x.to_string() + &String::from("nd");
-    }else if x % 10 == 3 {
+    } else if x % 10 == 3 {
         return x.to_string() + &String::from("rd");
     }
     x.to_string() + &String::from("th")

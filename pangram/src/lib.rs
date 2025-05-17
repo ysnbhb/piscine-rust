51a3723986e51418a754e70428cc8b534ebcbecb
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-17 12:20:55 +0100

    good

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,13 +1,13 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 pub fn is_pangram(s: &str) -> bool {
     if s.trim().is_empty() {
         return false;
     }
     let mut mp: HashMap<char, isize> = HashMap::new();
-    for i in s.as_bytes() {
-        if i.is_ascii_alphabetic() {
-            mp.insert(*i as char, 0);
+    for i in s.chars() {
+        if i <= 'a' && i > 'z' {
+            mp.insert(i, 0);
         }
     }
     mp.len() == 26

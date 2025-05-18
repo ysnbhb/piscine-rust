49f60dbc3e078e4ea4380918bb19baa780399e66
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-05-18 11:43:27 +0100

    good

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn scytale_cipher(message: String, i: u32) -> String {
+    let len = (message.len() as f64 / i as f64).ceil() as usize;
+    let mut grid = vec![vec![' '; i as usize]; len as usize];
+    for (j, c) in message.chars().enumerate() {
+        let row = j / i as usize;
+        let col = j % i as usize;
+        grid[row][col] = c
+    }
+    let mut res = String::new();
+    for r in 0..i {
+        for j in 0..len {
+            res.push(grid[j][r as usize]);
+        }
+    }
+
+    res
+}
---
+++ scytale_cipher/src/main.rs
@@ -0,0 +1,6 @@
+use scytale_cipher::*;
+
+fn main() {
+    println!("\"scytale Code\" size=6 -> {:?}", scytale_cipher(String::from("scytale Code"), 6));
+    println!("\"scytale Code\" size=8 -> {:?}", scytale_cipher(String::from("scytale Code"), 8));
+}
